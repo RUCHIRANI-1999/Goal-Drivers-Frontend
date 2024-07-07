f7c790f0fd31152ea1ed5cbd84bd1e9fa09b69aa
Ruchirani-1999 <ruchiranidakshina@gmail.com>
2024-07-07 21:58:48 +0530

    Updated Ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import NavHome from '../NavBar/NavHome';
 import Foot from '../Footer/Foot';
@@ -6,12 +6,34 @@ import Foot from '../Footer/Foot';
 export default function Home() {
   return (
     <div>
-      <div><NavHome/></div>
-      <div style={{height:'39.65vh'}}>
-        <Link to='/customer-bid'><button>Customer</button></Link>
-        <Link to='/seller'> <button>Seller</button></Link>
-        </div> 
-      <div><Foot/></div>
+      <NavHome />
+      <Container style={{ minHeight: '70vh' }} className="d-flex align-items-center justify-content-center">
+        <Row>
+          <Col md="6" className="mb-4">
+            <Card className="text-center">
+              <Card.Body>
+                <FaUser size={50} className="mb-3" />
+                <Card.Title>Customer</Card.Title>
+                <Link to='/customer-bid'>
+                  <Button color="primary">Go to Customer</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="6" className="mb-4">
+            <Card className="text-center">
+              <Card.Body>
+                <FaStore size={50} className="mb-3" />
+                <Card.Title>Seller</Card.Title>
+                <Link to='/seller'>
+                  <Button color="secondary">Go to Seller</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      <Foot />
     </div>
   );
 }
